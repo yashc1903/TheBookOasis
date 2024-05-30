37c50331d9835a506b9522fb0aaff59eb0d3fa00
yashc1903 <116756929+yashc1903@users.noreply.github.com>
2024-05-30 13:42:36 +0530

    login and signup pages created

---
+++ client/src/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LOgin() {
+  return (
+    <div>
+        {/* create the form here  */}
+    </div>
+  )
+}
+
+export default LOgin
---
+++ client/src/Pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+        {/* create the form here  */}
+    </div>
+
+  )
+}
+
+export default Signup
